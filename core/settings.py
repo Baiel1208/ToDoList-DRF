2f918e22828c265173dd8b9f6a3cafca14cc2b8b
Baiel1208 <baiel000005@gmail.com>
2023-07-29 12:42:43 +0600

    update settings and add urls.py

--- core/settings.py
+++ core/settings.py
@@ -42,7 +42,31 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #rest
+    'rest_framework',
+    # 'django_filters',
+
+    #apps
+    'todolist',
+    'users',
+
+    # admin
+    # 'jazzmin',
+    # 'django.contrib.admin',
+
+    # docs
+    # 'drf_yasg'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    # 'PAGE_SIZE': 10,
+    # 'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}  
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
